c703863a06adbfbc5eabcefcf7a813d8a868e4df
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 23:55:34 +0800

    refactor: namespace hiwepy→easy4j (copyright 残留)

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Spring Boot Starter For Maven Invoker
 
 ``` xml
 <dependency>
-	<groupId>io.github.hiwepy</groupId>
+	<groupId>io.github.easy4j</groupId>
 	<artifactId>maven-invoker-spring-boot-starter</artifactId>
 	<version>${project.version}</version>
 </dependency>
--- src/main/java/org/apache/maven/spring/boot/ext/MavenInvokerTemplate.java
+++ src/main/java/org/apache/maven/spring/boot/ext/MavenInvokerTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -37,7 +37,7 @@ import org.springframework.util.Assert;
 
 /**
  *	 基于Maven Invoker的Maven build实现，依赖于本机环境中的Maven环境
- * 	@author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * 	@author ： <a href="https://github.com/easy-4-java">hiwepy</a>
  */
 public class MavenInvokerTemplate {
 
