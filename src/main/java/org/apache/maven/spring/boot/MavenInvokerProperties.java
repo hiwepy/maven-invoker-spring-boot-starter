b7eb8f1056a8fa65df32e010e5d172e13989b181
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:54:21 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/apache/maven/spring/boot/MavenInvokerProperties.java
+++ src/main/java/org/apache/maven/spring/boot/MavenInvokerProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -31,7 +31,7 @@ import org.springframework.util.StringUtils;
 /**
  * Maven Invoker 参数配置
  * 
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @ConfigurationProperties(MavenInvokerProperties.PREFIX)
 public class MavenInvokerProperties {
